b4d56ef3ab31d0e2437392402c6be99bbb2aa2a1
Tade Salverda <tadesalverda@outlook.com>
2025-03-10 16:17:20 +0100

    rewrote CICD for nix

--- shared/script.nix
+++ shared/script.nix
@@ -10,14 +10,14 @@ let
         cd "${REPO_PATH}" || exit
 
     # Fetch updates from the remote repository without changing local files
-        git fetch "$REMOTE_NAME"
+        ${pkgs.git}/bin/git fetch "$REMOTE_NAME"
 
     # Check if there are new commits in the main branch (modify 'main' as needed)
-          NEW_COMMITS=$(git log --oneline "HEAD..${REMOTE_NAME} /main" | wc -l)
+          NEW_COMMITS=$(${pkgs.git}bin/git log --oneline "HEAD..${REMOTE_NAME} /main" | wc -l)
 
           if [ "$NEW_COMMITS" -gt 0 ]; then
           echo "There are new commits. Pulling changes..."
-          git pull origin main
+          ${pkgs.git}/bin/git pull origin main
           echo "Pull successful."
           else
           echo "No new commits detected."
