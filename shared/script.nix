c8eaaec3226796e1a0a389b5e252b6f679c3065d
Tade Salverda <tadesalverda@outlook.com>
2025-03-10 16:32:16 +0100

    wip

--- shared/script.nix
+++ shared/script.nix
@@ -13,7 +13,7 @@ let
         ${pkgs.git}/bin/git fetch "$REMOTE_NAME"
 
     # Check if there are new commits in the main branch (modify 'main' as needed)
-          NEW_COMMITS=$(${pkgs.git}/bin/git log --oneline "HEAD..${REMOTE_NAME} /main" | wc -l)
+          NEW_COMMITS=$(${pkgs.git}/bin/git log --oneline "HEAD..${REMOTE_NAME}/main" | wc -l)
 
           if [ "$NEW_COMMITS" -gt 0 ]; then
           echo "There are new commits. Pulling changes..."
@@ -40,7 +40,6 @@ in
       User = "admin";
     };
     script = fetchScript;
-    confinement.packages = pkgs.git;
   };
   systemd.timers."run-CICD" = {
     wantedBy = [ "timers.target" ];
