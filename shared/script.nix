d341d1bee09770bacebec8e01a7ba5a32d9de89a
Tade Salverda <tadesalverda@outlook.com>
2025-03-10 16:42:13 +0100

    wip

--- shared/script.nix
+++ shared/script.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }:
 let
-        # Configuration
-  REPO_PATH = "/home/admin/Magi";
-  REMOTE_NAME = "origin";
   fetchScript = ''
     #!/bin/bash
+        # Configuration
+        REPO_PATH = "/home/admin/Magi";
+        REMOTE_NAME = "origin";
 
     # Change directory to your repository
-        cd "${REPO_PATH}" || exit
+        cd "$REPO_PATH" || exit
 
     # Fetch updates from the remote repository without changing local files
         ${pkgs.git}/bin/git fetch "$REMOTE_NAME"
 
     # Check if there are new commits in the main branch (modify 'main' as needed)
-          NEW_COMMITS=$(${pkgs.git}/bin/git log --oneline "HEAD..${REMOTE_NAME}/main" | wc -l)
+          NEW_COMMITS=$(${pkgs.git}/bin/git log --oneline "HEAD..$REMOTE_NAME/main" | wc -l)
 
           if [ "$NEW_COMMITS" -gt 0 ]; then
           echo "There are new commits. Pulling changes..."
