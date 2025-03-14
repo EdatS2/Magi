896a62d1a44e09eed38babe0b65ef077ba1ef2f3
Tade Salverda <tadesalverda@outlook.com>
2025-03-14 18:20:06 +0100

    wip

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -118,23 +118,23 @@ in
     firewall.enable = false;
     nameservers = [ kubeGateway ];
   };
-  # services.kubernetes = {
-  #   # disabled kubernetes to focus on DNS and networking first
-  #   roles = [ "node" ];
-  #   masterAddress = kubeMasterHostname;
-  #   apiserverAddress = "https://${kubeMasterHostname}:${toString kubeAPILBport}";
-  #   easyCerts = true;
-  #   pki = {
-  #       enable = true;
-  #       # todo add extra san
-  #       cfsslAPIExtraSANs = kubeNodes;
-  #   };
-  #   apiserver = {
-  #     securePort = kubeMasterAPIServerPort;
-  #     advertiseAddress = kubeMasterIP;
-  #   };
-  #   addons.dns.enable = true;
-  # };
+  services.kubernetes = {
+    # disabled kubernetes to focus on DNS and networking first
+    roles = [ "node" ];
+    masterAddress = kubeMasterHostname;
+    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeAPILBport}";
+    easyCerts = true;
+    pki = {
+        enable = true;
+        # todo add extra san
+        cfsslAPIExtraSANs = kubeNodes;
+    };
+    apiserver = {
+      securePort = kubeMasterAPIServerPort;
+      advertiseAddress = kubeMasterIP;
+    };
+    addons.dns.enable = true;
+  };
   virtualisation.docker.enable = true;
   users.users.admin = {
     isNormalUser = true;
