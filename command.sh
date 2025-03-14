a92237d4879e87ef79ed95ce0d0ca04797ceed8d
Tade Salverda <tadesalverda@outlook.com>
2025-03-14 16:53:33 +0100

    wip

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
-nix run github:nix-community/nixos-anywhere -- --flake ".#balthazar" --generate-hardware-config nixos-generate-config ./hardware-configuration.nix --target-host nixos@192.168.88.130
+nix run github:nix-community/nixos-anywhere -- --flake ".#melchior-kube" --generate-hardware-config nixos-generate-config ./hardware-configuration.nix --target-host nixos@192.168.88.124
 
--- unique/melchior.nix
+++ unique/melchior.nix
@@ -5,11 +5,11 @@ pkgs
 {
    networking = {
        hostName = "melchior-kube"; 
-       vlans.kubernetes.interface = "eno1"; 
-       interfaces.kubernetes.ipv4.addresses = [{ address = "10.13.13.2";
-       prefixLength = 24; }];       
         # Kijk in de installer welke interface het gaat worden en stel dat dan
         # goed in.
+       vlans.kubernetes.interface = "ens18"; 
+       interfaces.kubernetes.ipv4.addresses = [{ address = "10.13.13.2";
+       prefixLength = 24; }];       
    };
    programs.zsh.shellAliases =  {
       rebuild =
