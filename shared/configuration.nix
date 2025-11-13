f6f2ab10781c4517ffb914891dd48b3f6a2274da
Tade Salverda <tadesalverda@outlook.com>
2025-11-13 16:05:53 +0100

    Revert "remove secrets"
    This reverts commit b9fcfbe6ef586be397517170879e6c2781b015c2.

--- shared/configuration.nix
+++ shared/configuration.nix
@@ -11,8 +11,13 @@ with builtins;  with pkgs.lib;
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./script.nix
-    ./steam.nix
-    ];
+  ] ++ (
+  if (machines.${config.system.name}.nvidia)
+  then [ 
+    steam.nix
+  ] else
+  []
+  );
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
