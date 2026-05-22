13e375208e8631487791564949ec535878fd51b0
Tade Salverda <tadesalverda@outlook.com>
2026-05-22 21:43:17 +0200

    steam

--- shared/steam.nix
+++ shared/steam.nix
@@ -1,126 +1,115 @@
-#steam_server
-{ lib, pkgs, machines, config, ... }:
-let 
-    cfg = config.steam_server.enable;
+# Headless Steam + Sunshine streaming server
+{ lib, pkgs, config, ... }:
+let
+  cfg = config.steam_server;
 in
 {
-    options.steam_server = {
-        enable = lib.mkEnableOption "Enable Steam server";
-    };
+  options.steam_server = {
+    enable = lib.mkEnableOption "Enable Steam headless streaming server";
+  };
 
-    config = lib.mkIf cfg {
-         # X and audio
+  config = lib.mkIf cfg.enable {
+    # --- Audio (Pipewire/WirePlumber) ---
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
-    programs.uwsm = {
-        enable = true;
-        waylandCompositors = {
-        hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "${pkgs.hyprland}/bin/Hyprland";
-        };
-       sway = {
-          prettyName = "Sway";
-          comment = "Sway compositor managed by UWSM";
-          binPath = "${pkgs.sway}/bin/sway";
-        };
-       } ;
-    };
-    xdg.portal.enable = true;
-    xdg.portal.wlr.enable = true;
-    programs.hyprland.enable = true;
+    services.pipewire.enable = true;
+    services.pipewire.alsa.enable = true;
+    services.pipewire.alsa.support32Bit = true;
+    services.pipewire.wireplumber.enable = true;
 
-    programs.sway = {
-        enable = true;
-        extraSessionCommands = ''
-            export DESKTOP_SESSION="sway"
-            export XDG_CURRENT_DESKTOP="sway"
-            export XDG_SESSION_DESKTOP="sway"
-            export XDG_SESSION_TYPE="wayland"
-            export WLR_BACKENDS="headless,libinput"
-            # OpenGL Variables
-            export GBM_BACKEND=nvidia-drm
-            export __GL_GSYNC_ALLOWED=0
-            export __GL_VRR_ALLOWED=0
-            export __GLX_VENDOR_LIBRARY_NAME=nvidia
-# Xwayland compatibility
-            export XWAYLAND_NO_GLAMOR=1
-        '';
-        };
-    
-    # Sunshine user, service and config 
+    # --- Headless Wayland compositor (labwc) ---
+    # Runs in the sunshine user's default session
+    systemd.user.services.labwc = {
+      enable = true;
+      serviceConfig = {
+        Environment = [
+          "WLR_BACKENDS=headless,libinput"
+          "WLR_LOG=verbose"
+          "GBM_BACKEND=nvidia-drm"
+          "__GL_GSYNC_ALLOWED=0"
+          "__GL_VRR_ALLOWED=0"
+          "__GLX_VENDOR_LIBRARY_NAME=nvidia"
+        ];
+        ExecStart = "${pkgs.labwc}/bin/labwc";
+        Restart = "always";
+        RestartSec = 3;
+      };
+      wantedBy = [ "default.target" ];
+    };
+
+    # --- Sunshine user ---
     users.users.sunshine = {
-        isNormalUser  = true;
-        home  = "/home/sunshine";
-        description  = "Sunshine Server";
-        extraGroups  = [ "wheel" "networkmanager" "input" "video" "sound"];
-        openssh.authorizedKeys.keys = [
+      isNormalUser = true;
+      home = "/home/sunshine";
+      description = "Sunshine Server";
+      extraGroups = [ "wheel" "networkmanager" "input" "video" "sound" ];
+      openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPsp/GP+FOMXJmr34gO5055gqvlAF7Q/QK72XXBIa6O tadesalverda@outlook.com"
       ];
-        hashedPassword =
+      hashedPassword =
         "$y$j9T$yqwYRrZy5iBJVX86iejJq.$.kautymt0ch6mTqgS95z19BqA9cUczPbd1rjM3PtRG7";
     };
 
-    services.sunshine = {
-        enable = true;
-        autoStart = true;
-        capSysAdmin = true;
-        package = pkgs.sunshine.override{ cudaSupport = true;};
-        settings = {
-          sunshine_name = "nixos";
-          capture = "wlr";
-          output_name = "3";
-        };
-        applications = {
-          env = {
-            PATH = "$(PATH):$(HOME)/.local/bin";
-          };
-          apps = [
-            {
-              name = "Steam";
-              # prep-cmd = [
-              #   {
-              #     # do = ''${pkgs.bash}/bin/sh -c "${pkgs.sway}/bin/swaymsg output HEADLESS-1 enable;
-              #     # ${pkgs.sway}/bin/swaymsg output HEADLESS-1 mode
-              #     # ${builtins.getEnv "SUNSHINE_CLIENT_WIDTH"}x${"SUNSHINE_CLIENT_HEIGHT"}@${builtins.getEnv
-              #     # "SUNSHINE_CLIENT_FPS"}Hz;
-              #     # do = ''${pkgs.bash}/bin/sh -c "${pkgs.sway}/bin/swaymsg output HEADLESS-1 enable;
-              #     # ${pkgs.sway}/bin/swaymsg output HEADLESS-1 mode
-              #     # 1920x1080@60Hz;
-              #     # ${pkgs.sway}/bin/swaymsg output HDMI-1 disable"'';
-              #     # undo = "${pkgs.sway}/bin/swaymsg output HEADLESS-1 disable;
-              #     # ${pkgs.sway}/bin/swaymsg output HDMI-1 enable";
-              #   }
-              # ];
-              exclude-global-prep-cmd = "false";
-              auto-detach = "true";
-              detached = ["${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"];
-              image-path = "steam.png";
-            }
-          ];
-        }
-        ;
+    # Enable linger for sunshine user (user services start at boot)
+    systemd.services.enable-linger-sunshine = {
+      enable = true;
+      description = "Enable linger for sunshine user";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.systemd}/bin/loginctl enable-linger sunshine";
+      };
+      wantedBy = [ "multi-user.target" ];
     };
 
-    
-    # Avahi is used by Sunshine
+    # --- Sunshine service ---
+    services.sunshine = {
+      enable = true;
+      autoStart = true;
+      openFirewall = true;
+      capSysAdmin = true;
+      package = pkgs.sunshine.override { cudaSupport = true; };
+      settings = {
+        sunshine_name = "melchior";
+        capture = "wlr";
+        output_name = "3";
+        video_codec = "h264";
+        audio_codec = "opus";
+      };
+      applications = {
+        env = {
+          PATH = "$PATH:/home/sunshine/.local/bin";
+        };
+        apps = [
+          {
+            name = "Steam";
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
+            detached = [
+              "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"
+            ];
+            image-path = "steam.png";
+          }
+        ];
+      };
+    };
+
+    # Firewall for Sunshine (manual in case openFirewall doesn't cover all)
+    networking.firewall.allowedTCPPorts = [ 47984 47989 47990 48010 ];
+    networking.firewall.allowedUDPPorts = [ 47998 47999 48000 48002 48010 ];
+
+    # Avahi for discovery
     services.avahi.publish.userServices = true;
 
-    # Required to simulate input
+    # Input simulation
     boot.kernelModules = [ "uinput" ];
-
-    # Maybe not necessary ? udev rules are ignored with ssh ?
     services.udev.extraRules = ''
       KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
     '';
 
-    # Force stop udisks2 (conflict with Gnome)
+    # Force stop udisks2
     services.udisks2.enable = lib.mkForce false;
 
     # Steam
     programs.steam.enable = true;
-
-    # Enable OpenGL
-    };
+  };
 }
